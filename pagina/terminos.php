7b905086dbd24f4b4fcafb8ff1a41e67175a394c
azafra98 <55493440+azafra98@users.noreply.github.com>
2019-12-18 19:52:14 +0100

    Update terminos.php
    Cambios en los terminos para los derechos de autor

--- pagina/terminos.php
+++ pagina/terminos.php
@@ -20,7 +20,31 @@
                 Correo electrónico: contacto (arroba) momazos (punto) combr/><br/>
                 Datos registrales: Registro Mercantil de Lleida, Tomo 1.252, Folio 151, Hoja L24.996, Inscripción 1.
             </p>
-            <h3>2. Aceptación de los Términos del Servicio y Normas de la Comunidad que a continuación se
+            <h3>2. Derechos de autor</h3>
+                <h4><a href="https://www.cuantocabron.com/">1. Cuanto Cabron</a></h4>
+                <p>Usted acepta que en caso de ser publicados dichos Archivos de Usuario, serán puestos a libre disposición 
+                del resto de usuarios del Sitio Web, sin limitación alguna.</p>
+                <h4><a href="https://www.cabroworld.com/">2. Cabroworld</a></h4>
+                <p>No debes hacer un uso no autorizado o fraudulento del portal y/o de sus contenidos con 
+                fines o efectos ilícitos.</p>
+                <h4><a href="https://9gag.com/">3. 9 Gag</a></h4>
+                <p>SUJETO A CUALQUIER DERECHO DE TERCEROS EN CUALQUIER CONTENIDO PREVIO QUE ES PARTE DE SU CONTENIDO DE USUARIO, CONSERVE LA PROPIEDAD DE CUALQUIER DERECHO QUE PUEDA TENER EN SU CONTENIDO DE USUARIO Y PUBLICAR SU CONTENIDO DE USUARIO NO TRANSFIERE LA PROPIEDAD DE SUS DERECHOS A 9GAG O CUALQUIER TERCERO .</p>
+                <h4><a href="https://www.instagram.com/">4. Instagram</a></h4>
+                <p>Según las Condiciones de uso y las Normas comunitarias de Instagram, solo puedes publicar contenido en Instagram si este no vulnera los derechos de propiedad intelectual de terceros. La mejor forma de saber con seguridad que el contenido que publicas en Instagram no infringe la ley sobre derechos de autor es publicar únicamente contenido que hayas creado tú mismo.
+                También podrías utilizar contenido de otra persona en Instagram si tienes su permiso (por ejemplo, mediante una licencia) o si el uso que realices está contemplado por la doctrina de uso legítimo u otro tipo de excepción de las leyes sobre derechos de autor. En general, es conveniente obtener permiso antes de publicar contenido, y que sea por escrito.
+                Ten en cuenta que Instagram no te puede ayudar a obtener permiso para utilizar contenido sujeto a derechos de autor.</p>
+                <h4><a href="https://es.memedroid.com/">5. Memedroid</a></h4>
+                <p>El Sitio Web no permite las actividades infractoras de los derechos de autor en su Sitio Web, y la EMPRESA tiene la potestad de borrar todos los contenidos y envíos de usuario que infrinjan estos derechos.</p>
+                <h4><a href="https://www.pccomponentes.com/">6. Pccomponentes</a></h4>
+                <p>Todos los signos distintivos, marcas, nombres comerciales, contenidos, estructura, diseño y forma de presentación de los elementos y cualquier otra información que aparezca en este sitio Web son propiedad de PCCOMPONENTES por sí o como cesionaria y están protegidos por los derechos de propiedad industrial e intelectual.
+                El usuario tiene prohibida la reproducción, transformación, distribución, comunicación pública y, en general cualquier otra forma de explotación de los elementos referidos en el apartado anterior sin autorización expresa de PCCOMPONENTES.</p>
+                <h4><a href="https://www.freepik.es/">7. Freepik</a></h4>
+                <p>Freepik respeta los derechos de autor de terceros, y nuestra política es responder a todas las notificaciones sobre infracción de derechos de autor según lo establecido por la normativa aplicable. Una vez que se recibe la notificación que cumple con estas Directrices, Freepik puede eliminar o desactivar el acceso al material presuntamente infractor o adoptar medidas adicionales que consideremos apropiadas, incluida la desactivación de la cuenta de un usuario. Al tomar tales medidas, Freepik intentará contactar a la parte que publicó el contenido para que puedan hacer cualquier declaración que consideren apropiada de acuerdo con las regulaciones aplicables.</p>
+                <h4><a href="index.php">8. Momazos</a></h4>
+                <p>Como dueños del sitio nos hemos decantado por tener Las licencias Creative Commons para permitir elegir los términos y condiciones de la licencia de una obra de la manera que mejor satisfaga al titular de los derechos. Por tal motivo, estas licencias han sido entendidas por muchos como una manera en que los autores pueden tomar el control de cómo quieren compartir su propiedad intelectual. Por ello usamos la de  Reconocimiento (CC BY)</p>
+
+
+            <h3>3. Aceptación de los Términos del Servicio y Normas de la Comunidad que a continuación se
                 detallan: </h3>
             <p>POR FAVOR LEA CON DETENIMIENTO LAS CONDICIONES DE USO ANTES DE UTILIZAR EL PRESENTE SITIO. PUEDEN
                 PARECERLE MUY TÉCNICAS Y FORMALES DESDE EL PUNTO DE VISTA LEGAL PERO SON DE VITAL IMPORTANCIA. AL
@@ -49,7 +73,7 @@
                 <li>La EMPRESA se reserva el derecho a modificar cualquier aspecto del Sitio Web en cualquier momento.
                 </li>
             </ol>
-            <h3>3. Normas de la Comunidad </h3>
+            <h3>4. Normas de la Comunidad </h3>
             <ol>
                 <li>El Usuario se compromete a cumplir con los términos y condiciones de estos Términos de Servicio,
                     Normas de la comunidad, y todas las leyes locales, nacionales y reglamentos internacionales.
@@ -92,7 +116,7 @@
                     actualizadas periódicamente. Le rogamos que periódicamente las consulte.
                 </li>
             </ol>
-            <h3>4. Propiedad intelectual e industrial del Sitio Web</h3>
+            <h3>5. Propiedad intelectual e industrial del Sitio Web</h3>
             <ol>
                 <li>La EMPRESA es titular de los derechos de propiedad intelectual e industrial, o ha obtenido las
                     autorizaciones o licencias correspondientes para su explotación, sobre el nombre de dominio, las
@@ -135,7 +159,7 @@
                     titulares de las mismas.
                 </li>
             </ol>
-            <h3>5. Propiedad Intelectual de los Archivos de Usuarios</h3>
+            <h3>6. Propiedad Intelectual de los Archivos de Usuarios</h3>
             <ol>
                 <li>Usted puede enviar imágenes y texto (“Comentarios de usuarios”) al Sitio Web. Las fotos, comentarios
                     o cualquier otra obra o material que incorporen los usuarios se conocen colectivamente como
@@ -168,7 +192,7 @@
                     todos los derechos de licencia otorgados en este documento.
                 </li>
             </ol>
-            <h3>6. Exclusión de responsabilidad</h3>
+            <h3>7. Exclusión de responsabilidad</h3>
             <ol>
                 <li>El Usuario entiende que al utilizar el Sitio Web, la EMPRESA no es responsable de la exactitud,
                     utilidad, seguridad o derechos de propiedad intelectual de o en relación con los Archivos de
@@ -193,7 +217,7 @@
                     caso de que existan dudas acerca del cumplimiento de las presentes condiciones de uso.
                 </li>
             </ol>
-            <h3>7. Legislación y jurisdicción aplicable</h3>
+            <h3>8. Legislación y jurisdicción aplicable</h3>
             <p>Las obligaciones, derechos y condiciones descritas en este documento están sometidas a la legislación
                 española, en caso de conflicto, los juzgados y Tribunales competentes serán los de Lucena.</p>
         </div>
