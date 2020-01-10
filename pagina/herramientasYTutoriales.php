87f343ddf7bd5f1e1745dbc49bff98dc930d1d46
Antonio Miguel Cobos Fernández <cobosmoriles@gmail.com>
2020-01-10 09:06:45 +0100

    Pequeños detalles control de video

--- pagina/herramientasYTutoriales.php
+++ pagina/herramientasYTutoriales.php
@@ -2,71 +2,6 @@
 <html lang="en">
 <head>
     <?php include("includes/head-tag-contents.php"); ?>
-    <style>
-        video {
-	width: 100%;
-	height: auto;
-}
-
-/*
-#video-container {
-	width: 640px;
-	height: 365px;
-	position: relative;
-}
-*/
-#video-controls {
-	position: absolute;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	padding: 5px;
-	opacity: 0;
-	-webkit-transition: opacity .3s;
-	-moz-transition: opacity .3s;
-	-o-transition: opacity .3s;
-	-ms-transition: opacity .3s;
-	transition: opacity .3s;
-	background-image: linear-gradient(bottom, #2B33EB, rgb(0,136,204) 100%);
-	background-image: -o-linear-gradient(bottom,#2B33EB, rgb(0,136,204) 100%);
-	background-image: -moz-linear-gradient(bottom, #2B33EB, rg#E8F4FA rgb(3,113,168) 13%, rgb(0,136,204) 100%);
-	background-image: -ms-linear-gradient(bottom, #2B33EB, rgb(0,136,204) 100%);
-
-	background-image: -webkit-gradient(
-		linear,
-		left bottom,
-		left top,
-		color-stop(0.13,#2B33EB),
-		color-stop(1, #2B33EB)
-	);
-}
-
-#video-container:hover #video-controls {
-	opacity: .9;
-}
-
-button {
-	background: #E8F4FA;
-	border: 0;
-	color: black;
-	-webkit-border-radius: 3px;
-	-moz-border-radius: 3px;
-	-o-border-radius: 3px;
-	border-radius: 3px;
-}
-
-button:hover {
-	cursor: pointer;
-}
-
-#seek-bar {
-	width: 360px;
-}
-
-#volume-bar {
-	width: 60px;
-}
-    </style>
 </head>
 <body>
 <?php include("includes/navigation.php"); ?>
@@ -92,35 +27,35 @@ button:hover {
             <div class="row" id="columna">
                 <h4>Tutorial de hacer memes</h4>
                 <div id="video-container">
-		            <!-- Video -->
-		            <video id="video">
-			            <source src="videos/ComoHacerMemesFacilmente_1.webm" type="video/webm">
-			            <source src="videos/ComoHacerMemesFacilmente.ogv" type="video/ogv">
-			            <source src="videos/ComoHacerMemesFacilmente.mp4" type="video/mp4">
-			            <p>
-				            Your browser doesn't support HTML5 video.
-				            <a href="videos/ComoHacerMemesFacilmente.mp4">Download</a> the video instead.
-			            </p>
-		            </video>
-		            <!-- Video Controls -->
-		            <div class="row" id="video-controls">
-			            <div class="col-sm-1">
-				            <button type="button" id="play-pause" class="play">Play</button>
-			            </div>
-			            <div class="col-sm-7">
-				            <input type="range" id="seek-bar" value="0">
-			            </div>
-			            <div class="col-sm-1">
-				            <button type="button" id="mute">Mute</button>
-			            </div>
-			            <div class="col-sm-2">
-				            <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
-			            </div>
-			            <div class="col-sm-1">
-				            <button type="button" id="full-screen">Full</button>
-			            </div>
-		            </div>
-	            </div>
+                    <!-- Video -->
+                    <video id="video">
+                        <source src="videos/ComoHacerMemesFacilmente_1.webm" type="video/webm">
+                        <source src="videos/ComoHacerMemesFacilmente.ogv" type="video/ogv">
+                        <source src="videos/ComoHacerMemesFacilmente.mp4" type="video/mp4">
+                        <p>
+                            Your browser doesn't support HTML5 video.
+                            <a href="videos/ComoHacerMemesFacilmente.mp4">Download</a> the video instead.
+                        </p>
+                    </video>
+                    <!-- Video Controls -->
+                    <div class="row" id="video-controls">
+                        <div class="col-l-1">
+                            <button type="button" id="play-pause" class="play">Play</button>
+                        </div>
+                        <div class="col-l-7">
+                            <input type="range" id="seek-bar" value="0">
+                        </div>
+                        <div class="col-l-1">
+                            <button type="button" id="mute">Mute</button>
+                        </div>
+                        <div class="col-l-2">
+                            <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
+                        </div>
+                        <div class="col-l-1">
+                            <button type="button" id="full-screen">Full</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="col-sm-4" id="derecha">
