e0d0b4e1b7eea6dba191b55282bdf3ccca9408a9
Antonio Miguel Cobos Fernandez <cobosmoriles@gmail.com>
2020-01-20 09:16:50 +0100

    Index dinamico

--- pagina/content.php
+++ pagina/content.php
@@ -1,3 +1,6 @@
+<?php
+include ("funciones/contenido/funciones_contenido.php");
+?>
 <div class="container">
     <div class="row">
         <div class="col-md-8 memes">
@@ -6,181 +9,32 @@
                     <h3 class="enlace"><a href="memeSemana.php">¿Quieres ver el meme de la semana? Pulsa aquí</a></h3>
                 </div>
             </div>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/corte_ingles.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.cabroworld.com/cabronazi/">Fuente: www.cabroworld.com/cabronazi/</a>
+            <?php
+            $contenido = getContenidoLast();
+            foreach ($contenido as $c) {
+                ?>
+                <a class="enlaceAMeme" href="meme.php">
+                    <div class="card">
+                        <img src="images/memes/<?php echo $c->imagen; ?>" alt="Sample Image" class="card-image"/>
+                        <div class="row">
+                            <div class="col text-left">
+                                <a class="fuente" href="<?php echo $c->fuente; ?>">Fuente:
+                                    <?php echo $c->fuente; ?></a>
+                            </div>
                         </div>
+                        <p class="card-content"><?php echo $c->descripcion; ?></p>
                     </div>
-                    <p class="card-content">A veces, las grandes superficies nos dejan en bandeja imagenes dignas
-                    de compratir en internet. Este es uno de esos casos y le damos las gracias a esta superficie.</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/tiempo_descraga.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">No olvideis pagar siempre el internet. MOROSOS.</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/strak.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.cuantocabron.com/">Fuente: www.cuantocabron.com/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/habitacion.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.instagram.com/cabronazi/">Fuente: www.instagram.com/cabronazi/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">-Mamá que ya he echo la cama<br>
-                    -Eso lo tendré que ver yo</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/reloj.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.instagram.com/kodevimresmi/">Fuente: www.instagram.com/kodevimresmi/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">¿Alguien mas piensa que habría sido mas fácil mover el reloj de sitio?<br>
-                    El ser humano siempre se supera a si mismo</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/halo2.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">Eso era posiblemente lo mas doloroso que nos podía pasar de pequeños.</p>
-                </div>
-            </a><a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/jenga.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">Como si no fuera dificil jugarlo en una mesa.</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/pasadizo.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/quimica.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/teachers.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/trump.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">Imaginamos que muchos de nosotros hemos pensado lo mismo.</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/wifi.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://es.memedroid.com/memes/">Fuente: es.memedroid.com/memes/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/buho.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.cabroworld.com/cabronazi/">Fuente: www.cabroworld.com/cabronazi/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">Los animalitos siempre son buenos para un meme...</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/buzz.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.cuantocabron.com/">Fuente: www.cuantocabron.com/</a>
-                        </div>
-                    </div>
-                    <p class="card-content">¿Se piensan que este gobierno va a ser una maldita revolución o que?</p>
-                </div>
-            </a>
-            <a class="enlaceAMeme" href="meme.php">
-                <div class="card">
-                    <img src="images/memes/index/juntamos.jpg" alt="Sample Image" class="card-image"/>
-                    <div class="row">
-                        <div class="col text-left">
-                            <a class="fuente" href="https://www.cuantocabron.com/">Fuente: www.cuantocabron.com/</a>
-                        </div>
-                    </div>
-                    <p class="card-content"></p>
-                </div>
-            </a>
-
+                </a>
+                <?php
+            }
+            ?>
         </div>
         <div class="col-md-4 d-none d-md-block lateral">
             <div class="row">
                 <div class="col-12 aside">
                     <a class="enlaceAMeme" href="meme.php">
                         <h3 class="aside-title">Meme de la semana</h3>
-                        <img src="images/memes/index/min/pineapple.jpg" alt="Sample Image" class="item-image"/>
+                        <img src="images/memes/min/pineapple.jpg" alt="Sample Image" class="item-image"/>
                         <div class="row">
                             <div class="col text-right">
                                 <a class="fuente" href="https://9gag.com/">Fuente: www.9gag.com</a>
--- pagina/funciones/contenido/funciones_contenido.php
+++ pagina/funciones/contenido/funciones_contenido.php
@@ -4,12 +4,17 @@ require_once("funciones/Conexion.php");
 /**
  * Ordenado por el id que va a ser autoincrement
  */
-/*
+
 function getContenidoLast()
 {
-
+    $c = new Conexion();
+    $resultado = $c->query("SELECT * FROM contenido ORDER BY id desc LIMIT 10");
+    while ($objeto = $resultado->fetch(PDO::FETCH_OBJ)) {
+        $contenido[] = $objeto;
+    }
+    return $contenido;
 }
-*/
+
 /**
  * orderby rand()
  */
@@ -17,16 +22,11 @@ function getContenidoAleatorio()
 {
     $c = new Conexion();
     $resultado = $c->query("SELECT * FROM contenido ORDER BY RAND() LIMIT 10");
-
     while ($objeto = $resultado->fetch(PDO::FETCH_OBJ)) {
         $contenido[] = $objeto;
     }
-
     return $contenido;
-
 }
-
-//var_dump(getContenidoAleatorio());
 
 /**
  * @param $id del contenido
